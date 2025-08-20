61363cd1d864715cdf071ac22a59c2092180ce54
Cameron Oakley <oakley.cameronj@gmail.com>
2025-08-20 19:47:10 +0000

    Removed logging file and added log configuration logic to main.py.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import logging as log
 
-from src._logging_ import configure_project_logging
 from src.client import create_discord_client_instance
 from src.helpers import setup_media_directory
 
@@ -10,7 +9,12 @@ if __name__ == "__main__":
         This serves as the entry point to the Groovester application.
     """
     
-    configure_project_logging(log.DEBUG)
+    log.basicConfig(
+        datefmt="%Y-%m-%d %H:%M:%S",
+        filename="Groovester.log",
+        format="%(levelname)s,%(asctime)s,%(message)s",
+        level=log.DEBUG,
+    )
 
     # Try to start the Groovester's client thread.
     try:
