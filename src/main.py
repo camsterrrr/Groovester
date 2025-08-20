20d4eb34185d6a9420f7bfcb50f8d19cbefbec7f
Cameron Oakley <oakley.cameronj@gmail.com>
2025-08-20 19:41:39 +0000

    Removed unnecessary modules from main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,7 @@
-# Standard Python libraries
 import logging as log
-import os
 
-# 3rd party APIs.
-from dotenv import load_dotenv
-
-# Groovester-defined APIs.
-from src._logging_ import LogLevel, configure_project_logging
+from src._logging_ import configure_project_logging
 from src.client import create_discord_client_instance
-from src.Groovester import GroovesterEventHandler
 from src.helpers import setup_media_directory
 
 
